f7241406e00310905e8456db9028645102c0e20d
dvelho <97854578+dvelho@users.noreply.github.com>
2023-03-07 17:09:06 +0000

    adding 2-tdd

--- code-with-quarkus/src/main/java/org/acme/GreetingResource.java
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,20 @@
 package org.acme;
 
+import io.smallrye.common.annotation.NonBlocking;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @NonBlocking
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactiver";
     }
 }
