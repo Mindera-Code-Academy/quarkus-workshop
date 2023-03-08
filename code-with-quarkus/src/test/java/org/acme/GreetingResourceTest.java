2e192675c9ea4fb1a211ac41a22e4eb071101959
dvelho <97854578+dvelho@users.noreply.github.com>
2023-03-08 23:02:53 +0000

    adding 2-tdd

--- code-with-quarkus/src/test/java/org/acme/GreetingResourceTest.java
+++ code-with-quarkus/src/test/java/org/acme/GreetingResourceTest.java
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactiver"));
+             .body(is("Hello from RESTEasy Surprise"));
     }
 
 }
